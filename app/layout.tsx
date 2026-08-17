8e979d4ac9fe3e0922369abccfe113cb37165632
luisjuniorawr1 <luis.junior.lima@gmail.com>
2026-08-17 16:27:00 -0300

    Load final horizontal homepage refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import './marketing-home.css';
 import './scroll-modes.css';
 import './marketing-home-refine.css';
 import './marketing-horizontal.css';
+import './marketing-horizontal-fixes.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://controols.com'),
